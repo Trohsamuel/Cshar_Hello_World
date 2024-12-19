f8778ed0cebad34e4844050c9d12a9c2189db014
Samuel TROH <mtroh@quantech.solutions>
2024-12-19 12:42:22 +0000

    Ajout du fichier Hello_World.cs

---
+++ Hello_World.cs
@@ -0,0 +1,11 @@
+using System;
+
+Console.WriteLine("Hello, World!");
+
+Console.Write("What is your name? ");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Nice to meet you, {name}!");
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
